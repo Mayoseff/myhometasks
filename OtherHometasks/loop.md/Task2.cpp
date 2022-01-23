5c3821fc118dc0d39fe63648e2cad7a360a31988
Mayoseff <90265659+Mayoseff@users.noreply.github.com>
2022-01-23 17:33:41 +0300

    Create Task2.cpp

---
+++ OtherHometasks/loop.md/Task2.cpp
@@ -0,0 +1,30 @@
+/*
+Задача 2.
+Для заданного значения n вычислить выражение: S = 1 + 1/2 + 1/4 + ... + 1/2^n
+*/
+
+#include <iostream>
+
+int main()
+{
+	double S = 1;
+	int k = 1;
+	int n;
+
+	std::cout << "n = ";
+	std::cin >> n;
+
+	while (k <= n)
+	{
+		int num = 1;
+		for (int i = 0; i < k; ++i)
+		{
+			num *= 2;
+		}
+		++k;
+		S += 1.0 / num;
+	}
+	std::cout << S;
+
+	return 0;
+}
